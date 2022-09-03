855b8d9a071ba294a21c0c33551b9217242ff91a
Roi Guiao <realrobert66@gmail.com>
2022-09-03 17:02:58 +0800

    Ronaldo comment

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
             {{ __('Home') }}
         </h2>
     </x-slot>
-
+{{-- ronaldo --}}
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             {{-- carousel --}}
